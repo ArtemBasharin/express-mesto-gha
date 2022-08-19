b237c0ccd0f440bc3c2c20018c8d91207d0f7f50
basharinaa <temon-bash@yandex.ru>
2022-08-19 20:31:28 +0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const AuthErr = require('../errors/AuthErr');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -40,13 +41,13 @@ userSchema.statics.findUserByCredentials = function findUser(email, password) {
     .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new AuthErr('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new AuthErr('Неправильные почта или пароль'));
           }
 
           return user;
