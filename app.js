68fa2998e7cffccd4a8568f1c9634eb413b373c0
basharinaa <temon-bash@yandex.ru>
2022-08-23 22:47:39 +0700

    logger added

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const mongoose = require('mongoose');
-
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const usersRouter = require('./routes/users');//
-const cardsRouter = require('./routes/cards');//
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 const auth = require('./middlewares/auth');
 const errHandler = require('./middlewares/errHandler');
 const { validateLogin, validateUser } = require('./middlewares/validation');
 const PageNotFound = require('./errors/PageNotFound');
-const { login, createUser } = require('./controllers/users');//
+const { login, createUser } = require('./controllers/users');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -20,11 +20,15 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(helmet());
+app.use(requestLogger);
+
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateUser, createUser);
 app.use(auth);
 app.use(usersRouter);
 app.use(cardsRouter);
+app.use(errorLogger);
+
 app.use('/', (req, res, next) => {
   next(new PageNotFound('Страница не найдена'));
 });
